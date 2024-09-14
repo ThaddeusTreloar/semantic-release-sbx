79b5bd870ba5c66655e3ef0096557998daf333ac
ThaddeusTreloar_Deloitte <ttreloar@deloitte.com.au>
2024-09-14 11:48:09 +1000

    feat(new-cluster): (platform-dev) Added cool cluster

--- script.sh
+++ script.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-FEAT="$(git log -n 1 HEAD --format=%B | ggrep -Po '(?<=feat\()[^)]+')"
-FIX="$(git log -n 1 HEAD --format=%B | ggrep -Po '(?<=fix\()[^)]+')"
-PERF="$(git log -n 1 HEAD --format=%B | ggrep -Po '(?<=perf\()[^)]+')"
-BREAKING="$(git log -n 1 HEAD --format=%B | ggrep -Po '(?<=BREAKING CHANGE: \()[^)]+')"
+MODULE=''
 
-if [ $FEAT ]; then
-    echo $FEAT
-fi
-if [ $FIX ]; then
-    echo $FIX
-fi
-if [ $PERF ]; then
-    echo $PERF
-fi
-if [ $BREAKING ]; then
-    echo $BREAKING
-fi
+GIT_LOG="$(git log -n 1 HEAD --format=%B)"
 
+MODULE="$MODULE$(echo "$GIT_LOG" | ggrep -Po '(?<=feat: \()[^)]+')"
+MODULE="$MODULE$(echo "$GIT_LOG" | ggrep -Po '(?<=fix: \()[^)]+')"
+MODULE="$MODULE$(echo "$GIT_LOG" | ggrep -Po '(?<=perf: \()[^)]+')"
+MODULE="$MODULE$(echo "$GIT_LOG" | ggrep -Po '(?<=BREAKING CHANGE: \()[^)]+')"
+
+echo $MODULE
