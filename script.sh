7ae5ba5a85a05cedff3932c46a5d4b0833ef9922
ThaddeusTreloar_Deloitte <ttreloar@deloitte.com.au>
2024-09-14 11:15:12 +1000

    BREAKING CHANGE: (platform-dev), my update to dev platform infra

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FEAT="$(git log -n 1 HEAD --format=%B | ggrep -Po '(?<=feat\()[^)]+')"
+FIX="$(git log -n 1 HEAD --format=%B | ggrep -Po '(?<=fix\()[^)]+')"
+PERF="$(git log -n 1 HEAD --format=%B | ggrep -Po '(?<=perf\()[^)]+')"
+BREAKING="$(git log -n 1 HEAD --format=%B | ggrep -Po '(?<=BREAKING CHANGE: \()[^)]+')"
+
+if [ $FEAT ]; then
+    echo $FEAT
+fi
+if [ $FIX ]; then
+    echo $FIX
+fi
+if [ $PERF ]; then
+    echo $PERF
+fi
+if [ $BREAKING ]; then
+    echo $BREAKING
+fi
