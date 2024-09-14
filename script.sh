ca2e7016ee4fd209540e70933ddf4ec36b4260cd
ThaddeusTreloar_Deloitte <ttreloar@deloitte.com.au>
2024-09-14 11:56:53 +1000

    asd feat(

--- script.sh
+++ script.sh
@@ -4,9 +4,10 @@ MODULE=''
 
 GIT_LOG="$(git log -n 1 HEAD --format=%B)"
 
-MODULE="$MODULE$(echo "$GIT_LOG" | ggrep -Po '(?<=feat: \()[^)]+')"
-MODULE="$MODULE$(echo "$GIT_LOG" | ggrep -Po '(?<=fix: \()[^)]+')"
-MODULE="$MODULE$(echo "$GIT_LOG" | ggrep -Po '(?<=perf: \()[^)]+')"
-MODULE="$MODULE$(echo "$GIT_LOG" | ggrep -Po '(?<=BREAKING CHANGE: \()[^)]+')"
+MODULE="$MODULE$(echo "$GIT_LOG" | ggrep '^feat(' | ggrep -Po '(?<=: \()[^)]+')"
+MODULE="$MODULE$(echo "$GIT_LOG" | ggrep '^fix(' | ggrep -Po '(?<=: \()[^)]+')"
+MODULE="$MODULE$(echo "$GIT_LOG" | ggrep '^perf(' | ggrep -Po '(?<=: \()[^)]+')"
+MODULE="$MODULE$(echo "$GIT_LOG" | ggrep '^BREAKING CHANGE:' | ggrep -Po '(?<=: \()[^)]+')"
 
 echo $MODULE
+
